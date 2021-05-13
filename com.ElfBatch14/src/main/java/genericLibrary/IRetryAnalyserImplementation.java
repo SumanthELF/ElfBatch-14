547b697437ae4d0ca7292e4081b381fa20696c49
Rajvi <Rajvi@LAPTOP-GT6VSVAU>
2021-05-13 16:55:54 +0530

    Adding IRetryAnalyser

--- com.ElfBatch14/src/main/java/genericLibrary/IRetryAnalyserImplementation.java
+++ com.ElfBatch14/src/main/java/genericLibrary/IRetryAnalyserImplementation.java
@@ -4,10 +4,10 @@ import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
 
 public class IRetryAnalyserImplementation implements IRetryAnalyzer {
-	
+
 	int count=0;
 	int retryCount=2;
-	
+
 	public boolean retry(ITestResult result) {
 		if(retryCount>=count) {
 			count++;
