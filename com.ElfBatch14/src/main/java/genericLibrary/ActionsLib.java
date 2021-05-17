178654737ace6c2b5a8f0ae30b11110113aabf92
anbuglu <84001519+anbuglu@users.noreply.github.com>
2021-05-17 08:05:47 +0530

    Update ActionsLib.java

--- com.ElfBatch14/src/main/java/genericLibrary/ActionsLib.java
+++ com.ElfBatch14/src/main/java/genericLibrary/ActionsLib.java
@@ -1,32 +1,35 @@
-package GenericLibrary;
+package genericLibrary;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
-public class ActionsLib {
+/*
+ *  @Author Rekha
+ */
+public class ActionsUtil {
 	
 	public WebDriver driver;
 	public Actions actions= new Actions(driver);
 	
-	public void getMoveToElement(WebElement element){
+	public void getMoveToElement(WebElement element) {
 		actions.moveToElement(element).perform();
 	}
 	
-	public void getMoveToElementCoOrd(WebElement element,int x,int y) {
-		actions.moveToElement(element,x,y).perform();
+	public void getMoveToCoOrdinate(WebElement element, int x, int y) {
+		actions.moveToElement(element, x, y).perform();
 	}
 	
-	public void moveByOffset(int x,int y) {
-		actions.moveByOffset(x,y).perform();
+	public void getMoveByOffset(int x, int y) {
+		actions.moveByOffset(x, y).perform();
 	}
 	
-	public void  getDragAndDrop(WebElement source,WebElement dest) {
-		actions.dragAndDrop(source, dest).perform();
+	public void getDragAndDrop(WebElement source, WebElement destination) {
+		actions.dragAndDrop(source, destination).perform();
 	}
 	
-	public void getDragAndDropBy(WebElement source,int x,int y) {
-		actions.dragAndDropBy(source,x,y).perform();
+	public void getDragAndDropBy(WebElement source, int x, int y) {
+		actions.dragAndDropBy(source, x, y).perform();
 	}
 	
 	public void getClickAndHold(WebElement source) {
@@ -37,15 +40,15 @@ public class ActionsLib {
 		actions.release(source).perform();
 	}
 	
-	public void  getDoubleClick(WebElement element){
+	public void  getDoubleClick(WebElement element) {
 		actions.doubleClick(element).perform();
 	}
 
-	public void  getContextClick(WebElement element){
+	public void  getContextClick(WebElement element) {
 		actions.contextClick(element).perform();
 	}
 	
-	public void getClick(WebElement element){
+	public void getClick(WebElement element) {
 		actions.click(element).perform();
 	}
 
